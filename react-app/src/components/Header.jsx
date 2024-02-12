2625a0987ce0649ef9f75118bb535fdbea608d8b
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2024-02-12 09:11:50 +0530

    36:52
    V-1 basic setup is done ,now actual work is about to start

---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+function Header()
+{
+    return (
+        <div>
+           <div className="header"><span className="mt-3">sell and Purchanse in you city</span></div>
+        </div>
+    );
+}
+
+export default Header;
