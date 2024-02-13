e34caacb4c4da2006c3bd3aa1261ac7b9186a70a
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2024-02-13 09:06:58 +0530

    V-2 36:00
    building login Api

--- node-app/index.js
+++ node-app/index.js
@@ -1,10 +1,24 @@
-
 const express = require('express')
 const app = express()
-const port = 3000
-
+const port = 4000
+var cors = require('cors') 
+app.use(cors())
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+const Users = mongoose.model('Users', { username: String , password:String});
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/signup' , (req , res)=>{
+  // const username = req.body.username;
+  // const password = req.body.password;
+  const user = new Users({username:"Dasdas" , password :"Dasdvcvxvas" });
+  user.save().then(() =>{
+    res.send("user saved")
+  }).catch(()=>{
+    res.send("some eroor occured")
+  })
 })
 
 app.listen(port, () => {
--- react-app/src/components/Login.jsx
+++ react-app/src/components/Login.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import Header from './Header';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
+import axios from 'axios';
 function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const handleApi =()=>{
+    const url = "http://localhost:4000/signup";
+    const data={username , password};
+    console.log({username , password});
+    axios.post(url , data).then((res)=>{
+      console.log(res);
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
   return (
     <div>
     <Header />
     welcome to login page
     <br />
     UserName
-    <input type="text" name="" id="" />
+    <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} id="" />
     <br />
     password
-    <input type="password" />
+    <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} id=""/>
     <br />
-        <button>Login</button>
+        <button onClick={handleApi}>Login</button>
     <Link to="/signup">signup</Link>
     </div>
   )
