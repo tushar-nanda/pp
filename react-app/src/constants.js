a6bdbd3541439ead8209c4b5f8582e2ac77a6491
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2024-03-10 12:21:59 +0530

    V9 partily done
    it is deployment part (did some study for easier deployment)

--- react-app/src/constants.js
+++ react-app/src/constants.js
@@ -1,2 +1,3 @@
-const API_URL = 'http://localhost:4000';
+const API_URL = process.env.NODE_ENV !='development' ? process.env.REACT_APP_BASR_URL : 'http://localhost:4000';
 export default API_URL;
+ 
