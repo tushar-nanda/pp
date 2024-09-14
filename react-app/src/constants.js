c40a43dce24f5242b84eb34f309a96a2d1d4f2a2
tushar-nanda <109102607+tushar-nanda@users.noreply.github.com>
2024-09-14 23:57:36 +0530

    frontend hosted

--- react-app/src/constants.js
+++ react-app/src/constants.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NODE_ENV !='development' ? process.env.REACT_APP_BASR_URL : 'http://localhost:4000';
+const API_URL = process.env.NODE_ENV !='development' ? process.env.REACT_APP_BASE_URL : 'http://localhost:4000';
 export default API_URL;
  
